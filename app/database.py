789a2cb42fe5fd892fd96088ba5a78b1d86ffd22
Argenika <angelalepeshko560@gmail.com>
2026-04-23 13:01:11 +0200

    fix database to postgres

--- app/database.py
+++ app/database.py
@@ -1,35 +1,8 @@
-import os
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
+import os
 
-from pathlib import Path
+# 🔥 Render ya te da esta variable automáticamente
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-env_path = Path(__file__).resolve().parent.parent / ".env"
-load_dotenv(dotenv_path=env_path)
-
-# 2️⃣ leer variables
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-# 3️⃣ DEBUG (AHORA sí)
-print("HOST:", DB_HOST)
-print("PORT:", DB_PORT)
-print("USER:", DB_USER)
-print("PASSWORD:", DB_PASSWORD)
-print("DB:", DB_NAME)
-
-# 4️⃣ conexión
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"ssl": {"ssl-mode": "REQUIRED"}}
-)
-try:
-    with engine.connect() as connection:
-        print("✅ CONECTADO A LA BASE DE DATOS")
-except Exception as e:
-    print("❌ ERROR:", e)
+# 🔥 conexión a la base de datos (PostgreSQL)
+engine = create_engine(DATABASE_URL)
