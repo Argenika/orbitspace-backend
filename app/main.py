05f9d51225e3fc664c6359576db7aa193d74fd70
Argenika <angelalepeshko560@gmail.com>
2026-04-25 18:50:23 +0200

    fix CORS config

--- app/main.py
+++ app/main.py
@@ -25,8 +25,8 @@ app.add_middleware(
         "https://orbitspace.vercel.app",
     ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type"],
 )
 
 security = HTTPBearer(auto_error=False)
